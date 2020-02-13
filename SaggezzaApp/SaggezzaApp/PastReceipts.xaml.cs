38dfd64c1da00a528b2094771d7bef54e0aaf64d
rogden341 <r.ogden@newcastle.ac.uk>
2020-02-13 15:53:18 +0000

    Added Home Page and Past Receipts

---
+++ SaggezzaApp/SaggezzaApp/PastReceipts.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SagezzaApp
+{
+    public partial class PastReceipts : ContentPage
+    {
+        public PastReceipts()
+        {
+            InitializeComponent();
+        }
+    }
+}
