e63a66e57003fdac1c05a581b576756be05202a5
Said Haj-Hassan <saidhajhassan@ymail.com>
2020-02-21 12:23:35 +0000

    Added Base Master Origin Project

--- SaggezzaApp/SaggezzaApp/App.xaml.cs
+++ SaggezzaApp/SaggezzaApp/App.xaml.cs
@@ -2,24 +2,27 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace SaggezzaApp {
-    public partial class App : Application {
-        public App() {
+namespace SaggezzaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
             InitializeComponent();
 
             MainPage = new MainPage();
         }
 
-        protected override void OnStart() {
-            // Handle when your app starts
+        protected override void OnStart()
+        {
         }
 
-        protected override void OnSleep() {
-            // Handle when your app sleeps
+        protected override void OnSleep()
+        {
         }
 
-        protected override void OnResume() {
-            // Handle when your app resumes
+        protected override void OnResume()
+        {
         }
     }
 }
